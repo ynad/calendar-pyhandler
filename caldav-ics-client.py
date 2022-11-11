36def2a6a5fa6eae719f5fc3135f348165164276
ynad <ynad92@gmail.com>
2022-11-11 17:45:28 +0100

    fix single full-day event creation, add date and time format checks, add some user prompt

--- caldav-ics-client.py
+++ caldav-ics-client.py
@@ -6,26 +6,39 @@
 # must provide JSON formatted: User-Agent, webdav server url, authentication
 # based on a NextCloud environment
 #
-# v0.3 - 2022.11.04 - https://github.com/ynad/caldav-py-handler
+# v0.4 - 2022.11.11 - https://github.com/ynad/caldav-py-handler
 # info@danielevercelli.it
 #
 
-import sys, os
-import logging
+import sys, os, logging
 import json
-#import pytz
 import click
 import requests
+from typing import Dict, List, Tuple
 from requests.auth import HTTPBasicAuth
-
 from icalendar import Calendar, Event, vCalAddress, vText
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
+
+
+
+###################################################################################################
+# USER SETTINGS - adjust to your environment
+domain="cloud.domain.com"
+user_conf_json="user_settings.json"
+logging_file="debug.log"
+prompt_wait=True
+
+# APP SETTINGS - no need to edit normally
+user_agent="caldav-ics-client"
+ics_file="tmp-event.ics"
+###################################################################################################
+
 
 
 # Enable logging
 logging.basicConfig(
-    filename="./debug.log",
+    filename=logging_file,
     filemode='w',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG
@@ -33,23 +46,53 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# global settings
-user_agent="caldav-ics-client"
-ics_file="tmp-event.ics"
-domain="cloud.domain.com"
-# user settings
-user_conf_json="user_settings.json"
+
+# show command syntax
+def show_syntax() -> str:
+    err = (
+        f"\nCaldav ICS CLIent - {domain}\n"
+        "=======================================\n\n"
+        f"Missing or wrong arguments! Syntax:\n{sys.argv}\n"
+        "    --name \"event name\"\n"
+        "    --descr \"event description\"\n"
+        "    --start_day \"event start day\"\n"
+        "    --end_day \"event end day\"\n"
+        "   [--start_hr \"event start hour\"]\n"
+        "   [--end_hr \"event end hour\"]\n"
+        "   [--loc \"event location\"]\n"
+        "   [--cal \"calendar to be used\"]\n"
+        "   [--invitee \"email to be invited\"]\n"
+    )
+    return err
 
 
 # load user settings from file
-def load_user_settings(user_conf_json):
+def load_user_settings(user_conf_json) -> dict:
     logger.debug("Loading user json settings from file: " + str(user_conf_json))
     with open(user_conf_json, 'r') as f:
         return json.load(f)
 
 
+# check string date format
+def check_date(date) -> Tuple[bool, str]:
+    try:
+        datetime.strptime(date, "%m/%d/%Y")
+    except ValueError as err:
+        return False, err
+    return True, ""
+
+
+# check string time format
+def check_time(time) -> Tuple[bool, str]:
+    try:
+        datetime.strptime(time, "%H:%M:%S")
+    except ValueError as err:
+        return False, err
+    return True, ""
+
+
 # create ICS file with provided event details
-def create_ics(user_settings, event_details):
+def create_ics(user_settings, event_details) -> None:
 
     # init calendar
     cal = Calendar()
@@ -94,7 +137,7 @@ def create_ics(user_settings, event_details):
 
 
 # make PUT request to upload ICS event file to given calendar
-def webdav_put_ics(user_settings, calendar, event_ics):
+def webdav_put_ics(user_settings, calendar, event_id) -> None:
 
     # check ICS existance
     if not os.path.exists(ics_file):
@@ -113,7 +156,7 @@ def webdav_put_ics(user_settings, calendar, event_ics):
 
     # make PUT request
     try:
-        res = requests.put(url=f"{user_settings['server']}/{user_settings['username']}/{calendar}/{event_ics}",
+        res = requests.put(url=f"{user_settings['server']}/{user_settings['username']}/{calendar}/{event_id}",
                             data=data,
                             headers={'Content-Type': 'text/calendar', 'User-Agent': user_agent},
                             auth = HTTPBasicAuth(user_settings['username'], user_settings['password']))
@@ -204,21 +247,46 @@ def main(name, descr, start_day, start_hr, end_day, end_hr, loc, cal, invite):
 
     # check command line arguments
     if not start_day or not end_day:
-        err = (
-            f"Missing arguments! Syntax:\n{sys.argv}\n"
-            + "    --name \"event name\"\n"
-            + "    --descr \"event description\"\n"
-            + "    --start_day \"event start day\"\n"
-            + "    --end_day \"event end day\"\n"
-            + "   [--start_hr \"event start hour\"]\n"
-            + "   [--end_hr \"event end hour\"]\n"
-            + "   [--loc \"event location\"]\n"
-            + "   [--cal \"calendar to be used\"]\n"
-            + "   [--invitee \"email to be invited\"]\n"
-        )
+        err = show_syntax()
+        #logger.error(err)
         print(err)
-        logger.error(err)
+        input("Press any key to exit.")
         return
+
+    # check date format
+    date_ok, date_err = check_date(start_day)
+    if not date_ok:
+        err = show_syntax()
+        logger.error(date_err)
+        print(f"{err}\n{date_err}\n")
+        input("Press any key to exit.")
+        return
+
+    date_ok, date_err = check_date(end_day)
+    if not date_ok:
+        err = show_syntax()
+        logger.error(date_err)
+        print(f"{err}\n{date_err}\n")
+        input("Press any key to exit.")
+        return
+
+    # check time format
+    if start_hr and end_hr:
+        time_ok, time_err = check_time(start_hr)
+        if not time_ok:
+            err = show_syntax()
+            logger.error(time_err)
+            print(f"{err}\n{time_err}\n")
+            input("Press any key to exit.")
+            return
+
+        time_ok, time_err = check_time(end_hr)
+        if not time_ok:
+            err = show_syntax()
+            logger.error(time_err)
+            print(f"{err}\n{time_err}\n")
+            input("Press any key to exit.")
+            return
 
     # load user settings from json file
     user_settings = load_user_settings(user_conf_json)
@@ -237,17 +305,32 @@ def main(name, descr, start_day, start_hr, end_day, end_hr, loc, cal, invite):
         event_details.update({'end' : datetime.strptime(f"{end_day} {end_hr}", "%d/%m/%Y %H:%M:%S")})
     # full day event
     else:
-        event_details.update({'start' : datetime.strptime(f"{start_day} 00:00:00", "%d/%m/%Y %H:%M:%S")})
-        event_details.update({'end' : datetime.strptime(f"{end_day} 23:59:59", "%d/%m/%Y %H:%M:%S")})
+        event_details.update({'start' : datetime.strptime(f"{start_day}", "%d/%m/%Y").date()})
+        event_details.update({'end' : datetime.strptime(f"{end_day}", "%d/%m/%Y").date() + timedelta(days=1)})
     # add invitees - to-do list of invitations
     if invite:
         event_details.update({'invite' : invite})
 
-    #print(event_details['uid'])
+    # wait for user confirmation, if enabled. To skip change 'prompt' to False
+    if prompt_wait:
+        print(f"\nCaldav ICS CLIent - {domain}\n"
+              "=======================================\n\n"
+              f"The following event details were provided:\n\n")
+        print(f"EVENT NAME:\t{name}\n"
+              f"DESCRIPTION:\t{descr}\n"
+              f"START DATE:\t{datetime.strftime(event_details['start'], '%d/%m/%Y %H:%M:%S')}\n"
+              f"END DATE:\t{datetime.strftime(event_details['end'], '%d/%m/%Y %H:%M:%S')}\n"
+              f"LOCATION\t{loc}\n"
+              f"CALENDAR:\t{event_details['calendar']}\n")
+        input("Press any key to confirm.")
 
+    # compile ICS file
     create_ics(user_settings, event_details)
 
+    # upload it to caldav server
     webdav_put_ics(user_settings, event_details['calendar'], event_details['uid'])
+
+    input("Press any key to exit.")
 
 
 if __name__ == '__main__':
